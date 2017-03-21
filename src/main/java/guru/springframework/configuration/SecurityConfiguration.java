341dbd99b4c247f100cc3d3934a2753fa11465f5
Alison <aacuna136@gmail.com>
2017-03-20 20:14:27 -0700

    Initial commit

---
+++ src/main/java/guru/springframework/configuration/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package guru.springframework.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by aacuna on 3/20/17.
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+}
